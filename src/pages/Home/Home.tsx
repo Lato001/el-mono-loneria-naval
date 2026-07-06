3f8ff2cfd77662ee09b53bf06c412b38f8fd2633
Lautaro Camejo <lautarocamejo@gmail.com>
2026-07-06 10:08:42 -0300

    add custom Brand Marquee

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,10 @@
-import { BrandMarquee } from "../../components/ui/Marquee/Marquee";
 import { Hero } from "../../components/layout";
-
+import { BrandMarquee } from "../../components/ui/Marquee/BrandMarquee";
 export function Home() {
   return (
     <>
       <Hero />
-      <BrandMarquee />
+      <BrandMarquee></BrandMarquee>
     </>
   );
 }
