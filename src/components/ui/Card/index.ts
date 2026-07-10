1aa4a82bed6b9ae9aa8c1a40ef0b04670535871d
Lautaro Camejo <lautarocamejo@gmail.com>
2026-07-10 01:01:26 -0300

    chore(card): remove SplitCardsSection from barrel

--- src/components/ui/Card/index.ts
+++ src/components/ui/Card/index.ts
@@ -1,5 +1,3 @@
 ﻿export { Card } from "./Card";
-export { SplitCardsSection } from "./SplitCardsSection";
 export { ReviewCard } from "./ReviewCard";
 export type { CardProps } from "./Card";
-export type { SplitCardsSectionProps } from "./SplitCardsSection";
