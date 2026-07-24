33676f7fd1409da7a35db39f391d1896525abbb8
Lautaro Camejo <lautarocamejo@gmail.com>
2026-07-23 21:48:28 -0300

    fix(ScrollToTOp): Scroll Function to Initialize al Windows Routes at Top

--- src/components/ui/Button/index.ts
+++ src/components/ui/Button/index.ts
@@ -1,5 +1,4 @@
 export { Button } from "./Button";
 export { WhatsappButton } from "./WhatsappButton";
 export {LinkButton} from "./LinkButton"
-export { ScrollToTop } from "./ScrollToTop";
 export type { ButtonProps, ButtonVariant, ButtonSize } from "./Button.types";
