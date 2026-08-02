a1f1534332e3e94fa2b413a46b24d19ea9bca392
Lautaro Camejo <lautarocamejo@gmail.com>
2026-08-02 00:09:10 -0300

    fix(media): MediaPlayer className + autoplay, BrandMarquee background

--- src/components/ui/MediaPlayer/MediaPLayer.tsx
+++ src/components/ui/MediaPlayer/MediaPLayer.tsx
@@ -7,8 +7,8 @@ interface MediaPlayerProps {
 
 function MediaPlayer({ src, className }: MediaPlayerProps) {
   return (
-    <div className={`w-full aspect-video ${className ?? ""}`}>
-      <ReactPlayer width="100%" height="100%" src={src} />
+    <div className={`w-full h-full aspect-video ${className ?? ""}`}>
+      <ReactPlayer loop muted autoPlay width="100%" height="100%" src={src} />
     </div>
   );
 }
